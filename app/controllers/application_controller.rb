a7d1f745ac3252b409c73863878be4adc015b34a
dsick <sick@davesick.net>
2016-05-11 09:05:19 -0700

    created roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
+  after_action :verify_authorized, except: :index
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
@@ -15,4 +19,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username]) 
 
   end
+
+  def user_not_authorized(exception)
+   policy_name = exception.policy.class.to_s.underscore
+
+   flash[:error] = t "#{policy_name}.#{exception.query}", scope: "pundit", default: :default
+   redirect_to(request.referrer || root_path)
+ end
 end
---
+++ app/policies/application_policy.rb
@@ -0,0 +1,53 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    skip_authorization
+  end
+
+  def show?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    user.standard?
+  end
+
+  def update?
+    user.present?
+  end
+
+  def edit?
+    user.present?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  def scope
+    Pundit.policy_scope!(user, record.class)
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope
+    end
+  end
+end
