33f577e1283c9106bbe270d0f643eae743a9b092
dsick <sick@davesick.net>
2016-05-11 17:36:03 -0700

    added test - still finishing

---
+++ spec/policies/wiki_policy_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+
+RSpec.describe WikiPolicy, type: :policy do
+  subject { WikiPolicy.new(user, wiki) }
+  let(:title) {Faker::Lorem.words}
+  let(:body) {Faker::Lorem.paragraph}
+  let(:private) {false}
+
+
+  let(:user) { create(:user) }
+
+  let(:wiki) { create(:wiki) }
+
+
+  context "for a visitor" do
+    let(:user) { nil }
+
+
+    it { should     permit(:show)    }
+
+    it { should_not permit(:create)  }
+    it { should_not permit(:new)     }
+    it { should_not permit(:update)  }
+    it { should_not permit(:edit)    }
+    it { should_not permit(:destroy) }
+  end
+
+  context "for a user" do
+    let(:user) { create(:user) }
+byebug
+    it { should permit(:show)    }
+    it { should permit(:create)  }
+    it { should permit(:new)     }
+    it { should permit(:update)  }
+    it { should permit(:edit)    }
+    it { should permit(:destroy) }
+  end
+end
